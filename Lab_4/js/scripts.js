0decb5cd2e5b9b5857ab1234689d57836fdb54bf
cholponzamir <50377750+cholponz@users.noreply.github.com>
2020-10-08 17:43:37 -0400

    lab 4 questions

--- Lab_4/js/scripts.js
+++ Lab_4/js/scripts.js
@@ -1,25 +1,68 @@
-let myFullName = "Bill Farmer"; // 10 characters in name...not including space
+
+
+
+
+
+
+let myFullName = "Cholpon Zamir"; // 10 characters in name...not including space
 // 1 Change the content of the div with class "header" to "[Your name]'s Lab 4"
 const myHeading = document.querySelector('.header');
-myHeading.textContent = "Cholpon's Lab 4";
+myHeading.textContent = myFullName+("'s Lab 4");
+
 
 // 2 Insert two paragraphs into the div with the class "content"
-//  Label each paragraph with a distinct class name
+
+
+
+let paragraph1=document.createElement('p');
+
+paragraph1.className=("content1");  //  Label each paragraph with a distinct class name
+
+let paragraph2=document.createElement('p1');
+
+paragraph2.className=("content1");
+
+Cont=document.querySelector(".content")
+
+Cont.append(paragraph1);
+
+Cont.append(paragraph2);
+
+
+
+
 
 
 // 3 Into the first paragraph, insert the phrase "my name has " length of your name " characters"
 //      (e.g. my name has 10 characters).
+lengthOfMyName=(myFullName.length)-1;//delete the space between first and last
+paragraph1.innerText="My name has "+lengthOfMyName+" characters";
+
 
 // 4 & 5 Into the second paragraph tag, return the 3rd character in your first name
 
+thirdCharacterOfMyName = myFullName.charAt(2);
+
+paragraph2.innerText=thirdCharacterOfMyName;
 // 6 Add a new line to your second paragraph
+//paragraph2.innerText="\n";
+//paragraph2.after('\n LA');
+
+paragraph2.innerText+="\n";
 
 // 7 Return the final three characters of your last name to that new line
+finalThreeLastName=myFullName.substr(-3);
 
+paragraph2.innerText+=finalThreeLastName;
 // 8 Substring your first and last name into two separate variables
+var firstName= myFullName.substr(-5);
+var lastName= myFullName.substr(0,7);
 
 // 9 Add the total length of your first and last names together
+totalNameCharacters=firstName.length+lastName.length;
 
 // 10 Display that total next to your name in your header
 
 
+
+myHeading.textContent += (" "+totalNameCharacters);
